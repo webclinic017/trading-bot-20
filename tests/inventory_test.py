3520521c93a11e82c3d40101608669f01ace381d
Asconius <joern.krusche@outlook.com>
2020-06-25 21:09:00 +0200

    inventory_test.py added

---
+++ tests/inventory_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.inventory import Inventory
+
+
+class InventoryTest(unittest.TestCase):
+    def test_init(self):
+        inventory = Inventory(1, 2)
+        self.assertEqual(inventory.number, 1)
+        self.assertEqual(inventory.price, 2)
+
+    def test_value(self):
+        inventory = Inventory(3, 5)
+        self.assertEqual(inventory.value(), 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
