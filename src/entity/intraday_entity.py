28aaf7a92943f38a5601b525925274149d9e5074
Asconius <joern.krusche@outlook.com>
2020-06-26 21:34:10 +0200

    intraday_entity.py modified

--- src/entity/intraday_entity.py
+++ src/entity/intraday_entity.py
@@ -3,10 +3,10 @@ from src import db
 
 class IntradayEntity(db.Model):
     __tablename__ = 'intraday'
-    date = db.Column(db.String(31), primary_key=True)
-    open = db.Column(db.String(31))
-    high = db.Column(db.String(31))
-    low = db.Column(db.String(31))
-    close = db.Column(db.String(31))
-    volume = db.Column(db.String(31))
-    ticker = db.Column(db.String(31), primary_key=True)
+    date = db.Column(db.String(20), primary_key=True)
+    open = db.Column(db.Float)
+    high = db.Column(db.Float)
+    low = db.Column(db.Float)
+    close = db.Column(db.Float)
+    volume = db.Column(db.Float)
+    ticker = db.Column(db.String(10), primary_key=True)
