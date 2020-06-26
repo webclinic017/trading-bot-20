0c9c58c1424b4c17a7e9c2ca466b62731b7bc30b
Asconius <joern.krusche@outlook.com>
2020-06-26 20:29:48 +0200

    forward_entity.py modified

--- src/entity/forward_entity.py
+++ src/entity/forward_entity.py
@@ -3,9 +3,9 @@ from src import db
 
 class ForwardEntity(db.Model):
     __tablename__ = 'forward'
-    date = db.Column(db.String(31), primary_key=True)
-    ticker = db.Column(db.String(31), primary_key=True)
-    action = db.Column(db.String(31))
-    price = db.Column(db.String(31))
-    number = db.Column(db.String(31))
-    cash = db.Column(db.String(31))
+    date = db.Column(db.String(20), primary_key=True)
+    ticker = db.Column(db.String(10), primary_key=True)
+    action = db.Column(db.String(10))
+    price = db.Column(db.Float)
+    number = db.Column(db.Integer)
+    cash = db.Column(db.Float)
