322f186deb97d1049333d1020a1f55fda78be0fc
Asconius <joern.krusche@outlook.com>
2020-06-26 22:31:34 +0200

    stock_entity.py modified

--- src/entity/stock_entity.py
+++ src/entity/stock_entity.py
@@ -3,5 +3,5 @@ from src import db
 
 class StockEntity(db.Model):
     __tablename__ = 'stock'
-    ticker = db.Column(db.String(31), primary_key=True)
-    isin = db.Column(db.String(31))
+    ticker = db.Column(db.String(10), primary_key=True)
+    isin = db.Column(db.String(20))
