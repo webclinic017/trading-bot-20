8de813b7cbd5fd3e9dfdd968acb9f654d9ee3050
Asconius <joern.krusche@outlook.com>
2020-07-16 22:07:34 +0200

    suite.py modified

--- tests/suite.py
+++ tests/suite.py
@@ -1,47 +1,5 @@
 import unittest
-from unittest.suite import TestSuite
 
-from tests.bo.analyser_bo_test import AnalyserBOTestCase
-from tests.bo.broker_bo_test import BrokerBOTestCase
-from tests.bo.forward_bo_test import ForwardBOTestCase
-from tests.bo.intraday_bo_test import IntradayBOTestCase
-from tests.bo.inventory_bo_test import InventoryBOTestCase
-from tests.bo.optimizer_bo_test import OptimizerBOTestCase
-from tests.bo.stock_bo_test import StockBOTestCase
-from tests.bo.strategy_bo_test import StrategyBOTestCase
-from tests.converter.attempt_dto_converter_test import AttemptDTOConverterTestCase
-from tests.dao.evaluation_dao_test import EvaluationDAOTestCase
-from tests.dao.forward_dao_test import ForwardDAOTestCase
-from tests.dao.intraday_dao_test import IntradayDAOTestCase
-from tests.dao.stock_dao_test import StockDAOTestCase
-from tests.dto.attempt_dto_test import AttemptDTOTestCase
-from tests.main_test import MainTestCase
-from tests.process_manager_test import ProcessManagerTestCase
-from tests.utils.utils_test import UtilsTestCase
-
-loader = unittest.TestLoader()
-suite: TestSuite = unittest.TestSuite()
-
-tests = [AnalyserBOTestCase,
-         AttemptDTOConverterTestCase,
-         AttemptDTOTestCase,
-         BrokerBOTestCase,
-         EvaluationDAOTestCase,
-         ForwardDAOTestCase,
-         ForwardBOTestCase,
-         IntradayBOTestCase,
-         IntradayDAOTestCase,
-         InventoryBOTestCase,
-         StockBOTestCase,
-         MainTestCase,
-         OptimizerBOTestCase,
-         ProcessManagerTestCase,
-         StockDAOTestCase,
-         StrategyBOTestCase,
-         UtilsTestCase]
-
-for test in tests:
-    suite.addTests(loader.loadTestsFromTestCase(test))
-
-runner = unittest.TextTestRunner(verbosity=3)
-result = runner.run(suite)
+suite = unittest.TestLoader().discover('.')
+runner = unittest.TextTestRunner()
+runner.run(suite)
