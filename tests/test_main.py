040eed8781065533562a973b6d6692ddfd03b478
Asconius <joern.krusche@outlook.com>
2020-07-21 16:09:15 +0200

    test_main.py modified

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,16 +10,17 @@ class MainTestCase(unittest.TestCase):
     @parameterized.expand([
         ('/', 200),
         ('/stock', 200),
+        ('/stock/intraday/ticker', 200),
         ('/intraday', 200),
         ('/evaluation', 200),
         ('/forward', 200),
-        ('/stock/intraday/ticker', 200),
         ('/process', 200),
         ('/process/start/process_name', 200),
         ('/process/stop/process_name', 200),
         ('/import', 200),
         ('/export', 200),
         ('/export/intraday', 200),
+        ('/configuration', 200),
         ('/invalid', 404),
     ])
     def test_get(self, path, code):
@@ -28,12 +29,23 @@ class MainTestCase(unittest.TestCase):
         response = client.get(path, environ_base={'REMOTE_ADDR': '127.0.0.1'})
         self.assertEqual(response.status_code, code)
 
-    def test_post(self):
+    def test_post_import_intraday(self):
         main.app.testing = True
         client = main.app.test_client()
         data = {'file': (io.BytesIO(b'{}'), 'intraday.json')}
         response = client.post('/import/intraday', environ_base={'REMOTE_ADDR': '127.0.0.1'}, data=data,
                                follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_configuration(self):
+        main.app.testing = True
+        client = main.app.test_client()
+        data = {'form_list-0-identifier': 'FORWARD_CASH',
+                'form_list-0-description': 'Forward Cash',
+                'form_list-0-value': '10000.00',
+                'submit': 'Submit'}
+        response = client.post('/configuration', environ_base={'REMOTE_ADDR': '127.0.0.1'}, data=data,
+                               content_type='application/x-www-form-urlencoded', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 
