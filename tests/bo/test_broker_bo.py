Asconius/trading-bot

Fixes #3: Predictor integration

tests/bo/test_broker_bo.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 from unittest import TestCase
 
-from src.bo.broker_bo import BrokerBO
-from src.bo.inventory_bo import InventoryBO
-from src.common.constants import ZERO
+from tradingbot.bo.broker_bo import BrokerBO
+from tradingbot.bo.inventory_bo import InventoryBO
+from tradingbot.common.constants import ZERO
 
 
 class BrokerBOTestCase(TestCase):

tests/bo/test_intraday_bo.py
@@ -4,13 +4,13 @@ from unittest.mock import patch
 from pandas import to_datetime, date_range
 from pytz import timezone
 
-from src import db, Utils
-from src.bo.intraday_bo import IntradayBO
-from src.common.constants import US_EASTERN
-from src.dao.intraday_dao import IntradayDAO
-from src.dao.stock_dao import StockDAO
-from src.entity.intraday_entity import IntradayEntity
 from tests.base_test_case import BaseTestCase
+from tradingbot import db, Utils
+from tradingbot.bo.intraday_bo import IntradayBO
+from tradingbot.common.constants import US_EASTERN
+from tradingbot.dao.intraday_dao import IntradayDAO
+from tradingbot.dao.stock_dao import StockDAO
+from tradingbot.entity.intraday_entity import IntradayEntity
 
 
 class IntradayBOTestCase(BaseTestCase):
@@ -42,7 +42,7 @@ class IntradayBOTestCase(BaseTestCase):
                 self.assertEqual(intraday.symbol, symbol)
 
     @patch('alpha_vantage.timeseries.TimeSeries.get_intraday_extended')
-    @patch('src.bo.stock_bo.StockBO.isin')
+    @patch('tradingbot.bo.stock_bo.StockBO.isin')
     def test_update(self, isin, intraday):
         isin.return_value = 'isin'
         intraday.return_value = self.get_intraday_csv('BBB')
@@ -63,7 +63,7 @@ class IntradayBOTestCase(BaseTestCase):
                                open=500, high=500, low=500, close=500, volume=500, symbol='AAA')
 
     @patch('alpha_vantage.timeseries.TimeSeries.get_intraday')
-    @patch('src.bo.stock_bo.StockBO.isin')
+    @patch('tradingbot.bo.stock_bo.StockBO.isin')
     def test_update_all_available(self, isin, intraday):
         isin.return_value = 'isin'
         intraday.return_value = self.get_intraday()

tests/common/test_scheduler.py
@@ -1,33 +1,33 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from src.common.scheduler import Scheduler
+from tradingbot.common.scheduler import Scheduler
 
 
 class SchedulerTestCase(TestCase):
 
-    @patch('src.common.scheduler.get')
+    @patch('tradingbot.common.scheduler.get')
     def test_update_table_intraday(self, get):
         Scheduler.update_table_intraday()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/update-table-intraday')
 
-    @patch('src.common.scheduler.get')
+    @patch('tradingbot.common.scheduler.get')
     def test_optimize(self, get):
         Scheduler.optimize()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/optimize')
 
-    @patch('src.common.scheduler.get')
+    @patch('tradingbot.common.scheduler.get')
     def test_forward(self, get):
         Scheduler.forward()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/forward')
 
-    @patch('src.common.scheduler.get')
+    @patch('tradingbot.common.scheduler.get')
     def test_init_database(self, get):
         Scheduler.init_database()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/init-database')
 
-    @patch('src.common.scheduler.Scheduler.init_database')
-    @patch('src.common.scheduler.run_pending', side_effect=RuntimeError())
+    @patch('tradingbot.common.scheduler.Scheduler.init_database')
+    @patch('tradingbot.common.scheduler.run_pending', side_effect=RuntimeError())
     def test_start(self, init_database, run_pending):
         try:
             Scheduler.start()

tests/dao/test_configuration_dao.py
@@ -1,11 +1,11 @@
 from decimal import Decimal
 
-from src import db
-from src.bo.configuration_bo import ConfigurationBO
-from src.dao.configuration_dao import ConfigurationDAO
-from src.entity.configuration_entity import ConfigurationEntity
-from src.enums.configuration_enum import ConfigurationEnum
 from tests.base_test_case import BaseTestCase
+from tradingbot import db
+from tradingbot.bo.configuration_bo import ConfigurationBO
+from tradingbot.dao.configuration_dao import ConfigurationDAO
+from tradingbot.entity.configuration_entity import ConfigurationEntity
+from tradingbot.enums.configuration_enum import ConfigurationEnum
 
 
 class ConfigurationDAOTestCase(BaseTestCase):

tests/dao/test_evaluation_dao.py
@@ -4,12 +4,12 @@ from unittest.mock import patch
 
 import pytz
 
-from src import db
-from src.dao.evaluation_dao import EvaluationDAO
-from src.dto.attempt_dto import AttemptDTO
-from src.entity.evaluation_entity import EvaluationEntity
-from src.enums.strategy_enum import StrategyEnum
 from tests.base_test_case import BaseTestCase
+from tradingbot import db
+from tradingbot.dao.evaluation_dao import EvaluationDAO
+from tradingbot.dto.attempt_dto import AttemptDTO
+from tradingbot.entity.evaluation_entity import EvaluationEntity
+from tradingbot.enums.strategy_enum import StrategyEnum
 
 
 class EvaluationDAOTestCase(BaseTestCase):
@@ -20,7 +20,7 @@ class EvaluationDAOTestCase(BaseTestCase):
     def setUpClass(cls):
         db.create_all()
 
-    @patch('src.utils.utils.Utils.now')
+    @patch('tradingbot.utils.utils.Utils.now')
     def setUp(self, now):
         self.truncate_tables()
         self.attempt = AttemptDTO(Decimal('1'), Decimal('2'), Decimal('3'), Decimal('4'), Decimal('5'), Decimal('6'))

webclinic017/trading-bot-20

tests/bo/test_broker_bo.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 from unittest import TestCase
 
-from src.bo.broker_bo import BrokerBO
-from src.bo.inventory_bo import InventoryBO
-from src.common.constants import ZERO
+from trading_bot.bo.broker_bo import BrokerBO
+from trading_bot.bo.inventory_bo import InventoryBO
+from trading_bot.common.constants import ZERO
 
 
 class BrokerBOTestCase(TestCase):

tests/bo/test_intraday_bo.py
@@ -4,13 +4,13 @@ from unittest.mock import patch
 from pandas import to_datetime, date_range
 from pytz import timezone
 
-from src import db, Utils
-from src.bo.intraday_bo import IntradayBO
-from src.common.constants import US_EASTERN
-from src.dao.intraday_dao import IntradayDAO
-from src.dao.stock_dao import StockDAO
-from src.entity.intraday_entity import IntradayEntity
 from tests.base_test_case import BaseTestCase
+from trading_bot import db, Utils
+from trading_bot.bo.intraday_bo import IntradayBO
+from trading_bot.common.constants import US_EASTERN
+from trading_bot.dao.intraday_dao import IntradayDAO
+from trading_bot.dao.stock_dao import StockDAO
+from trading_bot.entity.intraday_entity import IntradayEntity
 
 
 class IntradayBOTestCase(BaseTestCase):
@@ -42,7 +42,7 @@ class IntradayBOTestCase(BaseTestCase):
                 self.assertEqual(intraday.symbol, symbol)
 
     @patch('alpha_vantage.timeseries.TimeSeries.get_intraday_extended')
-    @patch('src.bo.stock_bo.StockBO.isin')
+    @patch('trading_bot.bo.stock_bo.StockBO.isin')
     def test_update(self, isin, intraday):
         isin.return_value = 'isin'
         intraday.return_value = self.get_intraday_csv('BBB')
@@ -63,7 +63,7 @@ class IntradayBOTestCase(BaseTestCase):
                                open=500, high=500, low=500, close=500, volume=500, symbol='AAA')
 
     @patch('alpha_vantage.timeseries.TimeSeries.get_intraday')
-    @patch('src.bo.stock_bo.StockBO.isin')
+    @patch('trading_bot.bo.stock_bo.StockBO.isin')
     def test_update_all_available(self, isin, intraday):
         isin.return_value = 'isin'
         intraday.return_value = self.get_intraday()

tests/common/test_scheduler.py
@@ -1,33 +1,33 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from src.common.scheduler import Scheduler
+from trading_bot.common.scheduler import Scheduler
 
 
 class SchedulerTestCase(TestCase):
 
-    @patch('src.common.scheduler.get')
+    @patch('trading_bot.common.scheduler.get')
     def test_update_table_intraday(self, get):
         Scheduler.update_table_intraday()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/update-table-intraday')
 
-    @patch('src.common.scheduler.get')
+    @patch('trading_bot.common.scheduler.get')
     def test_optimize(self, get):
         Scheduler.optimize()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/optimize')
 
-    @patch('src.common.scheduler.get')
+    @patch('trading_bot.common.scheduler.get')
     def test_forward(self, get):
         Scheduler.forward()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/forward')
 
-    @patch('src.common.scheduler.get')
+    @patch('trading_bot.common.scheduler.get')
     def test_init_database(self, get):
         Scheduler.init_database()
         get.assert_called_once_with('http://127.0.0.1:5000/process/start/init-database')
 
-    @patch('src.common.scheduler.Scheduler.init_database')
-    @patch('src.common.scheduler.run_pending', side_effect=RuntimeError())
+    @patch('trading_bot.common.scheduler.Scheduler.init_database')
+    @patch('trading_bot.common.scheduler.run_pending', side_effect=RuntimeError())
     def test_start(self, init_database, run_pending):
         try:
             Scheduler.start()

tests/dao/test_configuration_dao.py
@@ -1,11 +1,11 @@
 from decimal import Decimal
 
-from src import db
-from src.bo.configuration_bo import ConfigurationBO
-from src.dao.configuration_dao import ConfigurationDAO
-from src.entity.configuration_entity import ConfigurationEntity
-from src.enums.configuration_enum import ConfigurationEnum
 from tests.base_test_case import BaseTestCase
+from trading_bot import db
+from trading_bot.bo.configuration_bo import ConfigurationBO
+from trading_bot.dao.configuration_dao import ConfigurationDAO
+from trading_bot.entity.configuration_entity import ConfigurationEntity
+from trading_bot.enums.configuration_enum import ConfigurationEnum
 
 
 class ConfigurationDAOTestCase(BaseTestCase):

tests/dao/test_evaluation_dao.py
@@ -4,12 +4,12 @@ from unittest.mock import patch
 
 import pytz
 
-from src import db
-from src.dao.evaluation_dao import EvaluationDAO
-from src.dto.attempt_dto import AttemptDTO
-from src.entity.evaluation_entity import EvaluationEntity
-from src.enums.strategy_enum import StrategyEnum
 from tests.base_test_case import BaseTestCase
+from trading_bot import db
+from trading_bot.dao.evaluation_dao import EvaluationDAO
+from trading_bot.dto.attempt_dto import AttemptDTO
+from trading_bot.entity.evaluation_entity import EvaluationEntity
+from trading_bot.enums.strategy_enum import StrategyEnum
 
 
 class EvaluationDAOTestCase(BaseTestCase):
@@ -20,7 +20,7 @@ class EvaluationDAOTestCase(BaseTestCase):
     def setUpClass(cls):
         db.create_all()
 
-    @patch('src.utils.utils.Utils.now')
+    @patch('trading_bot.utils.utils.Utils.now')
     def setUp(self, now):
         self.truncate_tables()
         self.attempt = AttemptDTO(Decimal('1'), Decimal('2'), Decimal('3'), Decimal('4'), Decimal('5'), Decimal('6'))